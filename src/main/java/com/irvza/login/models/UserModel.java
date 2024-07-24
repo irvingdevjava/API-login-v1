32d12c4c20bd0a444c7eee5b9c492071b54a6d25
irvingdevjava <irvingspringboot@gmail.com>
2024-07-23 20:45:02 -0600

    Modelo usuario

---
+++ src/main/java/com/irvza/login/models/UserModel.java
@@ -0,0 +1,52 @@
+package com.irvza.login.models;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class UserModel implements Serializable{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String username;
+    @Column(nullable = false, unique = true)
+    private String password;
+    @Column(nullable = false)
+    private String role;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getRole() {
+        return role;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+
+}
